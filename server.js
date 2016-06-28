dafe7c333557a5cf8f4146cdeaa5a3351d496c5d
Matheus Bratfisch <matheus@ckl.io>
2016-06-28 12:23:52 -0300

    Adding automatic download capabilities

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').load({
 
 var request = require('request');
 var cheerio = require('cheerio');
+var mkdirp = require('mkdirp');
 var loginDetails = {
     email: process.env.PACKT_EMAIL,
     password: process.env.PACKT_PASSWORD,
@@ -11,6 +12,8 @@ var loginDetails = {
     form_id: "packt_user_login_form",
     form_build_id: ""
 };
+var fs = require('fs');
+var slugify = require('slugify');
 var url = 'https://www.packtpub.com/packt/offers/free-learning';
 var loginError = 'Sorry, you entered an invalid email address and password combination.';
 var getBookUrl;
@@ -20,6 +23,41 @@ var bookTitle;
 request = request.defaults({
     jar: true
 });
+
+function download(url, path, callback) {
+  request({uri: url})
+      .pipe(fs.createWriteStream(path))
+      .on('close', function() {
+        callback();
+      });
+}
+
+function downloadBookVersions(bookTitle, getBookUrl) {
+    bookTitle = slugify(bookTitle).toLowerCase();
+    match = getBookUrl.match(/\/(.*)\/(.*)\/(.*)/);
+    downloadUrl = 'https://www.packtpub.com/ebook_download/' + match[2];
+    codeUrl = 'https://www.packtpub.com/code_download/' + match[2];
+    pdfUrl = downloadUrl + '/pdf';
+    epubUrl = downloadUrl + '/epub';
+    mobiUrl = downloadUrl + '/mobi';
+
+    destFolder = 'downloads/' + bookTitle;
+    mkdirp(destFolder, function(err) {
+        if (err) {
+            console.log('----------- Packt Grab Done --------------');
+            return;
+        }
+
+        console.log('Downloading PDF URL: ' + downloadUrl + '/pdf');
+        console.log('Downloading EPUB URL: ' + downloadUrl + '/epub');
+        console.log('Downloading MOBI URL: ' + downloadUrl + '/mobi');
+
+        download(pdfUrl, destFolder + '/' + bookTitle + '.pdf', function () { console.log("Finished downloading PDF"); })
+        download(epubUrl, destFolder + '/' + bookTitle + '.epub', function () { console.log("Finished downloading EPUB"); })
+        download(mobiUrl, destFolder + '/' + bookTitle + '.mobi', function () { console.log("Finished downloading MOBI"); })
+
+    });
+}
 
 console.log('----------- Packt Grab Started -----------');
 request(url, function(err, res, body) {
@@ -70,7 +108,9 @@ request(url, function(err, res, body) {
 
             console.log('Book Title: ' + bookTitle);
             console.log('Claim URL: https://www.packtpub.com' + getBookUrl);
-            console.log('----------- Packt Grab Done --------------');
+
+            downloadBookVersions(bookTitle, getBookUrl);
+
         });
     });
 });
